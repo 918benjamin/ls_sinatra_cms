bd988a9728c64ea1ff6dc0cd1b3482844ea8ff67
Benjamin Perrault <benjamin.perrault@gmail.com>
2021-05-26 14:56:48 -0500

    Include Redcarpet and add helper methods to render html from markdown

--- Gemfile
+++ Gemfile
@@ -5,6 +5,7 @@ ruby "2.7.1"
 gem "sinatra", "~>1.4.7"
 gem "sinatra-contrib"
 gem "erubis"
+gem "redcarpet"
 
 gem "minitest"
 gem "rack-test"
--- cms.rb
+++ cms.rb
@@ -1,12 +1,29 @@
 require "sinatra"
 require "sinatra/reloader"
 require "tilt/erubis"
+require "redcarpet"
 
 root = File.expand_path("..", __FILE__)
 
 configure do
   enable :sessions
   set :session_secret, "secret"
+end
+
+def render_html(text)
+  markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML)
+  markdown.render(text)
+end
+
+def load_file_content(path)
+  file = File.read(path)
+  case File.extname(path)
+  when ".md"
+    render_html(file)
+  when ".txt"
+    headers["Content-Type"] = "text/plain"
+    file
+  end
 end
 
 get "/" do
@@ -20,8 +37,7 @@ get "/:file_name" do
   file_path = root + "/data/" + params[:file_name]
 
   if File.exist?(file_path)
-    headers["Content-Type"] = "text/plain"
-    File.read(file_path)
+    load_file_content(file_path)
   else
     session[:message] = "#{params[:file_name]} does not exist."
     redirect "/"
